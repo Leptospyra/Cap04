816f7b37417b295de02b67db4d17ffad2f3bcc9a
Rafael <ferreira.rfl@gmail.com>
2018-05-31 13:58:44 -0300

    Exer 1 somando fraçoes 1/n

--- src/Exercises_Cap04.java
+++ src/Exercises_Cap04.java
@@ -1,2 +1,14 @@
 public class Exercises_Cap04 {
+    public static void main(){
+        fractionSum(5);
+    }
+    /*Write a method called fractionSum that accepts an integer parameter n and
+    returns as a double the sum of the first n terms of the sequence*/
+    public static void fractionSum(int n){
+        double sum = 0.0;
+        for (int i = 1; i<=n; n++) {
+            sum += 1/n ;
+        }
+        System.out.println(sum + " é a soma das " + n +" primeiras frações");
+    }
 }
