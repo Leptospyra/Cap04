d69d4f459221088a1c75b3a3ad8b315d9b59a380
Rafael <ferreira.rfl@gmail.com>
2018-06-01 11:08:35 -0300

    Exer 08 e 09

--- src/Exercises_Cap04.java
+++ src/Exercises_Cap04.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Exercises_Cap04 {
     public static void main(String[] args){
         //fractionSum(10); // soma as n primeiras fraçoes
@@ -5,9 +7,110 @@ public class Exercises_Cap04 {
         //System.out.println(season(10,2)); //imprime a estação do ano
         //System.out.println(pow(3,4)); // 3 * 3 * 3 * 3, 81.0
         //printRange(5,5); //[19, 18, 17, 16, 15, 14, 13, 12, 11]
-        
+        // exercicio 06
+        Scanner console = new Scanner(System.in);
+        //String texto = smallestLargest(console);
+        //System.out.println(texto);
+        //fim do exercicio 06
+        //evenSumMax(console); exercicio 07
+        //System.out.println(printGPA(console)); // exercicio 8 entra dados na mesma linha
+        Exercicio09();
+    }
+    //aqui me parece que passar um objeto seria melhor, refazer quando aprender.
+    private static void Exercicio09() {
+        boolean triangulo;
+        triangulo = ValidaTriangulo(3,2,4);
+        if (triangulo) {
+            System.out.println(printTriangleType(3,2,4));
+        } else {
+            throw new IllegalArgumentException("Triangulo invalido");   //essa linha é diferente do exercicio 22, aqui eu
+                                                                        // trato o erro com exception
+        }
     }
 
+    private static boolean ValidaTriangulo(int cateto01, int cateto02, int hipotenusa) {
+        if ((Math.abs(cateto02 - hipotenusa) < (cateto01)) && ((cateto02 + hipotenusa) > (cateto01))){
+            if ((Math.abs(cateto01 - hipotenusa) < (cateto02)) && ((cateto01 + hipotenusa) > (cateto02))) {
+                if ((Math.abs(cateto01 - cateto02) < (hipotenusa)) && ((cateto01 + cateto02) > (hipotenusa))){
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+
+    //solução do livro (Exer22)
+    public static String printTriangleType(int s1, int s2, int s3) {
+        if (s1 == s2 && s2 == s3) {
+            return ("equilateral");
+        } else if (s1 == s2 || s1 == s3 || s2 == s3) {
+            return("isosceles");
+        } else {
+            return("scalene");
+        }
+    }
+
+    //exercicio 08
+    private static double printGPA(Scanner console) {
+        System.out.print("Enter a student record: ");
+        String nome = console.next();
+        int qtdeNotas = console.nextInt();
+        int sum = 0;
+        int cont = 0;
+        for (int i = 1; i<= qtdeNotas; i++){
+            sum = sum + console.nextInt();
+        }
+
+        return (1.0*sum/qtdeNotas);
+    }
+
+
+    /*The method should ask by prompt how many times its repeat. ok
+    * Once the user has entered all the integers, the method should print the sum of all the even numbers,
+    * along with the largest even number typed.
+    * You may assume that the user will type at least one nonnegative even integer.
+     */
+    private static void evenSumMax(Scanner console) {
+        int maior = -1000;
+        int menor = 1000;
+        System.out.println("Quantas numeros inteiros?");
+        int qtde = console.nextInt();
+        int soma = 0;
+        for (int i = 1; i <=qtde; i++) {
+            System.out.printf("numero %s:", i);
+            int numero = console.nextInt();
+            if (numero%2==0) {
+                if (numero > maior) {
+                    maior = numero;
+                    soma += numero;
+                }
+            }
+        }
+        System.out.printf("Even sum = %s, even max = %s",soma,maior);
+    }
+
+    private static String smallestLargest(Scanner console) {
+        int maior = -1000;
+        int menor = 1000;
+        for (int i = 1; i <=4; i++) {
+            System.out.printf("numero %s:", i);
+            int numero = console.nextInt();
+            if (numero > maior) {
+                maior = numero;
+            }
+            if (numero < menor) {
+                menor = numero;
+            }
+        }
+        return ("menor " + menor + "; " + "maior: " + maior);
+
+    }
+
+    /*The method called smallestLargest that accepts a Scanner for the console as a parameter and asks the user
+    to enter numbers, then prints the smallest and largest of all the numbers supplied by the user. You may assume that
+    the user enters a valid number greater than 0 for the number of numbers to read. Here is a sample execution:
+    */
 
     /*printRange accepts two integers as arguments
     * and prints the sequence of numbers between the two arguments, enclosed in square brackets.
